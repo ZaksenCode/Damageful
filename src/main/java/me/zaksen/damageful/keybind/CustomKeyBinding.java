5872743a7107ed20ea875caafa0987e9e566fd76
ZaksenCode <maksimsidorkin2006@mail.ru>
2024-06-09 08:12:59 +0300

    Add CustomKeyBinding

--- src/main/java/me/zaksen/damageful/keybind/CustomKeyBinding.java
+++ src/main/java/me/zaksen/damageful/keybind/CustomKeyBinding.java
@@ -1,4 +1,23 @@
 package me.zaksen.damageful.keybind;
 
-public class CustomKeyBinding {
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+
+import java.util.function.Consumer;
+
+public class CustomKeyBinding extends KeyBinding {
+
+    private final Consumer<MinecraftClient> function;
+
+    public CustomKeyBinding(String translationKey, InputUtil.Type type, int code, String category, Consumer<MinecraftClient> function) {
+        super(translationKey, type, code, category);
+        this.function = function;
+    }
+
+    public void process(MinecraftClient client) {
+        if(isPressed()) {
+            function.accept(client);
+        }
+    }
 }
